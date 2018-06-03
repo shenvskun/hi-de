15ad91441e74fd61ad4d7c426b200eb918736af3
eric2 <2496566169@qq.com>
2018-06-03 17:28:09 +0800

    房间公司一体

--- src/test/java/sk/base/collection/Learn_Collection.java
+++ src/test/java/sk/base/collection/Learn_Collection.java
@@ -2,8 +2,12 @@ package sk.base.collection;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.HashSet;
 import java.util.Iterator;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.Set;
 
 public class Learn_Collection {
 	public static void main(String[] args) {
@@ -49,5 +53,113 @@ public class Learn_Collection {
 		sortL.add(11);
 		Collections.sort(sortL);
 		System.out.println(sortL);
+		
+		System.out.println("===linkedList");
+		LinkedList linkedList = new LinkedList();
+		linkedList.add("dsd");
+		linkedList.add("dssssd");
+		linkedList.offer("aaa");
+		linkedList.remove("dd");
+		System.out.println(linkedList);
+		System.out.println(linkedList.pop());
+		
+		int ppp = 0;
+		int[] i = {3,4,ppp};
+		
+		System.out.println("============测试set===============");
+		Person p1 = new Person("p1", 11);
+		Person p2 = new Person("p2", 11);
+		Person p3 = new Person("p2", 11);
+		Person p4 = new Person("p4", 11);
+		Set<Person> treeSet = new HashSet();
+		treeSet.add(p1);
+		treeSet.add(p2);
+		treeSet.add(p3);
+		treeSet.add(p4);
+		System.out.println(treeSet.contains(p3));
+		System.out.println(treeSet);
+		for (Person p : treeSet) {
+			System.out.println(p);
+		}
+		
+		System.out.println("=======list特有的迭代器=============");
+		List testListIterator = new ArrayList();
+		testListIterator.add("第一本书");
+		testListIterator.add("第二本书");
+		testListIterator.add("第三本书");
+		testListIterator.add("第四本书");
+		ListIterator li = testListIterator.listIterator();
+		li.next();
+		li.remove();//删掉第一本  iterator的公共方法
+		System.out.println(testListIterator);
+		li.next();
+		li.set("dddd");
+		li.add("第五本书"); //在it.next后面添加一个元素
+		System.out.println(testListIterator);
+		li.next();
+		System.out.println(testListIterator);
+		
 	}
+}
+
+class Person {
+	private String name;
+	private Integer age;
+	
+	public Person() {
+	}
+	
+	@Override
+	public String toString() {
+		return "Person [name=" + name + ", age=" + age + "]";
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((age == null) ? 0 : age.hashCode());
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age == null) {
+			if (other.age != null)
+				return false;
+		} else if (!age.equals(other.age))
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+
+	public Person(String name, Integer age) {
+		this.name = name;
+		this.age = age;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAge() {
+		return age;
+	}
+	public void setAge(Integer age) {
+		this.age = age;
+	}
+	
 }
